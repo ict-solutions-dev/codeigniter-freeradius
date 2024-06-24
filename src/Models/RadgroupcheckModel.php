1769d0d764a3fe50e06b1e94894f0e3f3db36d64
Jozef Rebjak <jozefrebjak@icloud.com>
2024-06-24 10:35:19 +0200

    Update RadgroupcheckModel.php
    Signed-off-by: Jozef Rebjak <jozefrebjak@icloud.com>

--- src/Models/RadgroupcheckModel.php
+++ src/Models/RadgroupcheckModel.php
@@ -5,12 +5,9 @@ declare(strict_types=1);
 namespace IctSolutions\CodeIgniterFreeRadius\Models;
 
 use IctSolutions\CodeIgniterFreeRadius\Entities\Radgroupcheck;
-use IctSolutions\CodeIgniterFreeRadius\Traits\Sharable;
 
 class RadgroupcheckModel extends BaseModel
 {
-    use Sharable;
-
     protected $primaryKey    = 'id';
     protected $returnType    = Radgroupcheck::class;
     protected $allowedFields = [
