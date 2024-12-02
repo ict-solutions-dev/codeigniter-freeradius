51ca92617ee0e278ff452a339678547de1915ac8
Jozef Rebjak <jozefrebjak@icloud.com>
2024-12-02 09:53:53 +0100

    fix: remove redundant primary key addition for radusergroup table migration

--- src/Database/Migrations/2024-01-21-223112_create_radius_tables.php
+++ src/Database/Migrations/2024-01-21-223112_create_radius_tables.php
@@ -145,7 +145,6 @@ class CreateRadiusTables extends Migration
                 'groupname' => ['type' => 'VARCHAR', 'constraint' => 64, 'default' => ''],
                 'priority'  => ['type' => 'INT', 'constraint' => 11, 'unsigned' => true, 'default' => 1],
             ]);
-            $this->forge->addPrimaryKey('id');
             $this->forge->addKey('username');
             $this->forge->createTable($this->tables['radusergroup']);
         }
